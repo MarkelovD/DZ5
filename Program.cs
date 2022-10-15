7fe5f4610471387afa69e789aef9612284629b81
Dmitry <markelov16@gmail.com>
2022-10-15 17:37:47 +0500

    1 задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1 задача
+int [] test = GetArray(10, -9, 9);// вызов метода с массивом
+Console.WriteLine($"{String.Join(",",test)}");
+Console.Write("количество четных чисел = ");
+EvenNumber ();// вызов метода счета
+
+
+
+
+
+
+
+
+// метод счета четных чисел
+void EvenNumber (){
+    int count =0;
+for (int i=0;i<test.Length;i++){
+    if (test[i]%2 ==0){
+        count++;
+    }
+    }
+    Console.WriteLine($"{count}");
+}
+// метод заполнения массива
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
